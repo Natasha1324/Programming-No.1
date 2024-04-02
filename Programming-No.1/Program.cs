53d6dfaf1f431435aa8872b164133be78a2aa89d
Natasha1324 <165684802+Natasha1324@users.noreply.github.com>
2024-04-02 11:51:30 -1200

    Add project files.

---
+++ Programming-No.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programming_No._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
